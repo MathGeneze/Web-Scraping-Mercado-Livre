a2f7386bbe7f07a61418e01cfa1596ccb328727b
Matheus Geneze <matheus.geneze@gmail.com>
2026-01-07 21:29:51 -0300

    .

--- Estrutura/pages/visao_geral.py
+++ Estrutura/pages/visao_geral.py
@@ -1,5 +1,38 @@
 import streamlit as st
 
-st.subheader('Visão Geral')
 
 # Neste arquivo, vou explicar o que é o projeto, objetivo, tecnologias que usei, problemas que resolvi.
+
+
+
+
+st.title('Visão Geral')
+
+
+
+
+
+
+# * - Introdução
+# - Exemplifique o projeto em poucas linhas, sem entrar em código, foando na clareza, não na complexidade.
+
+
+# * - O problema que ele resolve
+# - Aqui posso explicar algo como: Necessidade de automação na coleta de informações públicas;
+# - Também em como é extreamamente complexo e dificil acessar a API da Mercado Livre.
+
+
+# * - Como o projeto funciona (fluxo geral)
+# - Sem código, apenas lógica.
+# - Posso usar um passo a passo ou até um fluxograma simples.
+
+
+# * - Tecnologias utilizadas
+# - Tecnologia + o papel dela no projeto.
+
+
+# * - Aprendizados e Objetivos Futuros
+# - O que aprendi com o projeto / O que pretendo melhorar.
+# - Ideias de melhorias futuras
+
+
